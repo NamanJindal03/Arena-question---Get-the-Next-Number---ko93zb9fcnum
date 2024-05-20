06d8368f008e03cc46b65b6f7a6981d2001ee17d
Naman Jindal <namanjindal113@gmail.com>
2024-05-20 22:50:44 +0530

    done

--- src/app.js
+++ src/app.js
@@ -4,6 +4,15 @@ const app = express();
 //Middlewares
 app.use(express.json());
 
+app.get('/api/get-next-num', (req, res) => {
+    const number = req.body.num;
+    if(isNaN(number)){
+        return res.json({message:"error" , status: 'failure'})
+    }
+    const newNumber = parseInt(number) + 1;
+    return res.json({message: newNumber, status: 'success'})
+})
+
 // Write a GET Request to get the next number from the input 'num'.
 // Endpoint : /api/get-next-num
 // Return the response as {message : , status: }
